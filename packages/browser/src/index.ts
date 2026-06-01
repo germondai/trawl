75dc6207ee3e50ea3858ac4776b0082b6f98c3ee
germondai <germondai@gmail.com>
2026-06-01 16:33:00 +0200

    feat: export browser package public API

---
+++ packages/browser/src/index.ts
@@ -0,0 +1,4 @@
+export { FINGERPRINT } from "./fingerprint"
+export type { BrowserHandle } from "./pool"
+export { BrowserPool, newFreshContext, PoolExhaustedError } from "./pool"
+export { SessionCache } from "./session"
